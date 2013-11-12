032bece1519a5eab460774319f7eeef8a312c3d9
dlachut1@umbc.edu <dlachut1@umbc.edu@fa38a5d4-7599-bbc8-a82d-156dfb954912>
2013-11-12 00:09:42 +0000

    For task two, can detect if a bitstring contains a specified word

--- trunk/src/aos.erl
+++ trunk/src/aos.erl
@@ -1,15 +1,41 @@
 -module(aos).
 
--export([longestWordLength/1,longestWord/1]).
+-export([longestWordLength/1,longestWord/1,wordInString/2,toLower/1]).
 
+%% splitPattern() is a private function to return a list of non-word characters
 splitPattern() ->
-    [<<" ">>,<<"\t">>,<<"\n">>,<<".">>,<<",">>,<<"\"">>].
+    [<<" ">>, <<"\t">>, <<"\n">>, <<".">>, <<",">>, <<"\"">>, <<"<">>, <<">">>,
+    <<"?">>, <<":">>, <<";">>, <<"[">>, <<"]">>, <<"{">>, <<"}">>, <<"=">>,
+    <<"+">>, <<"_">>, <<")">>, <<"(">>, <<"*">>, <<"&">>, <<"^">>, <<"%">>,
+    <<"$">>, <<"#">>, <<"@">>, <<"!">>, <<"`">>, <<"~">>, <<"1">>, <<"2">>,
+    <<"3">>, <<"4">>, <<"5">>, <<"6">>, <<"7">>, <<"8">>, <<"9">>, <<"0">>].
 
+%% toLower(S) converts ascii chars in a bitstring to lowercase
+toLower(S) ->
+    binary:list_to_bin(string:to_lower(binary:bin_to_list(S))).
+    
 %% longestWordLength(S) returns the bit-length of the longest word in S if S is 
 %% a bitstring
 longestWordLength(S) ->
-    lists:max([erlang:bit_size(X) || X <- binary:split(S,splitPattern(),[global])]).
+    lists:max([erlang:bit_size(X) || X <- binary:split(S,splitPattern(),
+    [global])]).
 
-%% longestWord returns the longest word in S if S is a bitstring
+%% longestWord(S) returns the longest word in S if S is a bitstring
 longestWord(S) ->
-    hd([X || X <- binary:split(S,splitPattern(),[global]), erlang:bit_size(X) == longestWordLength(S)]).
+    hd([X || X <- binary:split(S,splitPattern(),[global]), 
+    erlang:bit_size(X) == longestWordLength(S)]).
+
+%% wordInList(W,L) returns true if a bitstring word exists in a list of 
+%% bitstring words. This is a helper function for wordInString(S)
+wordInList(_,[]) ->
+    false;
+wordInList(W,[H|T]) ->
+    case toLower(W) == toLower(H) of
+        true -> true;
+        false -> wordInList(W,T)
+    end.
+    
+%% wordInString(W,S) returns true if word W is in bitstring S
+wordInString(W,S) ->
+    wordInList(W, [X || X <- binary:split(S,splitPattern(),[global]), 
+    erlang:bit_size(X) > 0]).
