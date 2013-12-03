ebc5d0017c92e0960f9d76e10e3540ebc0108a44
kaavya1@umbc.edu <kaavya1@umbc.edu@fa38a5d4-7599-bbc8-a82d-156dfb954912>
2013-12-03 02:48:20 +0000

    Modified to include bitstrings

--- readFromFile.erl
+++ readFromFile.erl
@@ -14,5 +14,6 @@
 readFile(File)->												
 	{ok,Binary}=file:read_file(File),						%Read the file contents to a binary variable
 	S = string:tokens(binary_to_list(Binary), ", \r\n\t."),	%Tokenizes the file contents based on new line, tab space and commas.
-	[X || X <- S].											%Creates a list from tokens.
+	[list_to_bitstring(X) || X <- S].						%Creates a list from tokens.
+
 
