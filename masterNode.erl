effc2d90c488f46392836a3535f4f0dd0a8b02c8
kaavya1@umbc.edu <kaavya1@umbc.edu@fa38a5d4-7599-bbc8-a82d-156dfb954912>
2013-12-03 02:47:35 +0000

    Changed to include the long and highest freq word

--- masterNode.erl
+++ masterNode.erl
@@ -21,9 +21,13 @@ start(File,Nodes)->
 Temp=fragments:split(File,Nodes), 						%It passes the file to be splitted and the no of nodes to start with.
 ResultS = Nodes - length(Temp), 						%The number of fragments created M is always less than the number of nodes so as to allow for replication. Hence computing the replication factor for the remaining nodes
 Temp2=fragments:replication(Temp,Temp,Nodes,ResultS),	%Produces one fragment for each node along with replication.
-printlength(Temp2), 									%This should be equal to the number of nodes.
-%S=string_Manip:longestWord(Temp),
-findLongWord(Temp2,length(Temp2)).
+%printlength(Temp2), 									%This should be equal to the number of nodes.
+S=fragments:longestWord(),
+io:format("~n~s is the longest word",[S]),
+Max=fragments:freqWord(),
+Word=erlang:hd(Max),
+io:format("~n~p Highest frequency length",[element(1,Word)]),
+io:format("~n~p Highest frequency word",[element(2,Word)]).
 %spawnproc(Temp2,Nodes,1,[]).							%Fragments are ready, time to start spanning.
 
 
@@ -35,11 +39,6 @@ findLongWord(Temp2,length(Temp2)).
 %	NewFragment = Fragments--hd[Fragments],				%Removes the fragment from list 
 %	spawnproc(NewFragments,Size-1,NEWPIDs).				%and proceeds with recursive call.
 
-
-findLongWord(List,LSize) when LSize>0 ->
-	Element = lists:nth(LSize,List),
-	io:format("The fragment size is ~s\n",[Element#fragment.f]),
-	findLongWord(List,LSize-1).
 
 printlength(Temp2)->									% The function prints the length of a list.
 io:format("~p.~n Length is ",[length(Temp2)]).
@@ -53,3 +52,4 @@ io:format("~p.~n Length is ",[length(Temp2)]).
  %{ok, S} = file:open(File, write),
   %      lists:foreach( fun(X) -> io:format(S, "~p.~n",[X]) end, Temp2),
    %     file:close(S).
+
