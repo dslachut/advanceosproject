6e872a6a45b4e2c66296546ddd2214a462697708
dlachut1@umbc.edu <dlachut1@umbc.edu@fa38a5d4-7599-bbc8-a82d-156dfb954912>
2013-12-04 20:32:17 +0000

    update worker and tests

--- trunk/src/aostest.erl
+++ trunk/src/aostest.erl
@@ -15,25 +15,34 @@ test() ->
     F4 = <<"in the mind to suffer">>,
     F5 = <<"slings and arrows of">>,
     F6 = <<"outrageous fortune,">>,
-    Node0 = spawn(worker,startNode,[F1,1,[],0,self()]),
-    spawn(worker,startNode,[F2,2,[#neighbor{pid=Node0,age=0}],0,self()]),
-    spawn(worker,startNode,[F4,4,[#neighbor{pid=Node0,age=0}],0,self()]),
-    spawn(worker,startNode,[F1,1,[#neighbor{pid=Node0,age=0}],0,self()]),
-    spawn(worker,startNode,[F2,2,[#neighbor{pid=Node0,age=0}],0,self()]),
-    spawn(worker,startNode,[F3,3,[#neighbor{pid=Node0,age=0}],0,self()]),
-    spawn(worker,startNode,[F4,4,[#neighbor{pid=Node0,age=0}],0,self()]),
-    spawn(worker,startNode,[F5,5,[#neighbor{pid=Node0,age=0}],0,self()]),
-    spawn(worker,startNode,[F6,6,[#neighbor{pid=Node0,age=0}],0,self()]),
+    Node0 = spawn(worker,startNode,[F1,1,[],0,self(),true]),
+    spawn(worker,startNode,[F2,2,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    spawn(worker,startNode,[F4,4,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    spawn(worker,startNode,[F1,1,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    spawn(worker,startNode,[F2,2,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    spawn(worker,startNode,[F3,3,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    spawn(worker,startNode,[F4,4,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    spawn(worker,startNode,[F5,5,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    spawn(worker,startNode,[F6,6,[#neighbor{pid=Node0,age=0}],0,self(),false]),
+    Node0 ! longestWord,
+    receive 
+        Msg -> io:format("Longword 1: ~p~n",[Msg])
+    after 
+        5000 -> io:format("~p~n",["Fail Longword"])
+    end,
     timer:sleep(10000),
     Node0 ! hello,
     Node0 ! neighbors,
+    timer:sleep(1000),
     Node0 ! longestWord,
+    timer:sleep(1000),
     receive 
-        Msg1 -> io:format("Longword: ~p~n",[Msg1])
+        Msg1 -> io:format("Longword 2: ~p~n",[Msg1])
     after 
         5000 -> io:format("~p~n",["Fail Longword"])
     end,
     Node0 ! {search, #search{word = <<"fortune">>, repeats=0}},
+    timer:sleep(1000),
     receive
         Msg2 -> io:format("Found 'fortune' at ~p~n",[Msg2])
     after
@@ -56,4 +65,5 @@ test() ->
         5000 -> io:format("~p~n",["Fail Search 2c"])
     end,
     timer:sleep(10000),
-    Node0 ! neighbors.
+    Node0 ! neighbors,
+    "".
