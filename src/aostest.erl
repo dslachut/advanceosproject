3d5a5c2126b524d1b8e11d844e41412b71c6d58e
dlachut1@umbc.edu <dlachut1@umbc.edu@fa38a5d4-7599-bbc8-a82d-156dfb954912>
2013-12-04 19:32:29 +0000

    Program to test the worker nodes.

---
+++ src/aostest.erl
@@ -0,0 +1,59 @@
+-module(aostest).
+
+-compile(export_all).
+
+-record(neighbor, {pid,age=100000}).
+
+-record(search, {word,
+                 repeats}).
+
+
+test() ->
+    F1 = <<"To be or not to be,">>,
+    F2 = <<"that is the question.">>,
+    F3 = <<"Whether it is nobler">>,
+    F4 = <<"in the mind to suffer">>,
+    F5 = <<"slings and arrows of">>,
+    F6 = <<"outrageous fortune,">>,
+    Node0 = spawn(worker,startNode,[F1,1,[],0,self()]),
+    spawn(worker,startNode,[F2,2,[#neighbor{pid=Node0,age=0}],0,self()]),
+    spawn(worker,startNode,[F4,4,[#neighbor{pid=Node0,age=0}],0,self()]),
+    spawn(worker,startNode,[F1,1,[#neighbor{pid=Node0,age=0}],0,self()]),
+    spawn(worker,startNode,[F2,2,[#neighbor{pid=Node0,age=0}],0,self()]),
+    spawn(worker,startNode,[F3,3,[#neighbor{pid=Node0,age=0}],0,self()]),
+    spawn(worker,startNode,[F4,4,[#neighbor{pid=Node0,age=0}],0,self()]),
+    spawn(worker,startNode,[F5,5,[#neighbor{pid=Node0,age=0}],0,self()]),
+    spawn(worker,startNode,[F6,6,[#neighbor{pid=Node0,age=0}],0,self()]),
+    timer:sleep(10000),
+    Node0 ! hello,
+    Node0 ! neighbors,
+    Node0 ! longestWord,
+    receive 
+        Msg1 -> io:format("Longword: ~p~n",[Msg1])
+    after 
+        5000 -> io:format("~p~n",["Fail Longword"])
+    end,
+    Node0 ! {search, #search{word = <<"fortune">>, repeats=0}},
+    receive
+        Msg2 -> io:format("Found 'fortune' at ~p~n",[Msg2])
+    after
+        5000 -> io:format("~p~n",["Fail Search 1"])
+    end,
+    Node0 ! {search, #search{word = <<"to">>, repeats=0}},
+    receive
+        Msg3 -> io:format("Found 'to' at ~p~n",[Msg3])
+    after
+        5000 -> io:format("Found 'to' at ~p~n",["Fail Search 2a"])
+    end,
+    receive
+        Msg4 -> io:format("Found 'to' at ~p~n",[Msg4])
+    after
+        5000 -> io:format("~p~n",["Fail Search 2b"])
+    end,
+    receive
+        Msg5 -> io:format("Found 'to' at ~p~n",[Msg5])
+    after
+        5000 -> io:format("~p~n",["Fail Search 2c"])
+    end,
+    timer:sleep(10000),
+    Node0 ! neighbors.
