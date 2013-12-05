1281f954e8917789536bdccc4d4f59e52469bd1a
kaavya1@umbc.edu <kaavya1@umbc.edu@fa38a5d4-7599-bbc8-a82d-156dfb954912>
2013-12-05 01:19:21 +0000

    Updated few areas.

--- src/readFromFile.erl
+++ src/readFromFile.erl
@@ -11,8 +11,9 @@
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
-readFile(File)->												
-	{ok,Binary}=file:read_file(File),						%Read the file contents to a binary variable
-	S = string:tokens(binary_to_list(Binary), ", \r\n\t."),	%Tokenizes the file contents based on new line, tab space and commas.
-	[list_to_bitstring(X) || X <- S].						%Creates a list from tokens.
+
+readFile(File)->
+{ok,Binary}=file:read_file(File), %Read the file contents to a binary variable
+S = string:tokens(binary_to_list(Binary), ", \r\n\t"), %Tokenizes the file contents based on new line, tab space and commas.
+[X || X <- S]. %Creates a list from tokens.
 
